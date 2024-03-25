44988c20780629cd3fb2fc8f4a225cb836d4114b
joao victor <128156864+joaovictordocarmo@users.noreply.github.com>
2024-03-25 09:07:00 -0300

    Add files via upload

---
+++ empresa.js
@@ -0,0 +1,15 @@
+    let programacao = ['Heloa', 'Maria Rafaela', 'Maria Eduarda', 'Diego', 'João Victor', 'Gabriel', 'Luana', 'Thauanne', 'Aline', 'Lucas']
+
+    let seguranca = ['Leonardo', 'Miguel', 'João Paulo', 'Rafael'];
+
+    seguranca.pop();
+    programacao.push("Raphael");
+
+    console.log("time de programacao:",programacao);
+    console.log("#########################")
+    console.log("time de seguranca:", seguranca);
+
+
+    //let empresa = fumcionarios.concat (seguranca)
+    //console.log(empresa)
+    
